9d9ef0047e91470206528de5ac005e738ff20a9c
nukopy <pytwbf201830@gmail.com>
2021-04-09 21:57:39 +0900

    update: modify options' order of pytest command

--- scripts/lint_and_test.sh
+++ scripts/lint_and_test.sh
@@ -15,4 +15,4 @@ isort app tests --check-only
 # test with pytest
 PYTHONPATH=./app
 # pytest --cov=app --cov=tests --cov-report=term-missing --cov-report=xml tests
-pytest -v tests --cov=app --junitxml=junit/test-results.xml --cov-report=xml --cov-report=html
+pytest -v tests --cov=app --cov-report=html --cov-report=xml --junitxml=junit/test-results.xml
