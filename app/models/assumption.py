4700b41475e7cdb73dde96a5ef47c803762aa6e3
nullnull <nullnullprogramming@gmail.com>
2021-04-10 17:11:57 +0900

    fix

--- app/models/assumption.py
+++ app/models/assumption.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, DateTime
 from .database import ENGINE, Base
 
 
