3bf5d6f4a863f7b338bc3feec17fbbbb67335cee
nullnull <nullnullprogramming@gmail.com>
2021-04-13 01:39:39 +0900

    add math-data

--- app/models/assumption/mathmatics_assumpton_data.py
+++ app/models/assumption/mathmatics_assumpton_data.py
@@ -1,1 +1,11 @@
-mathmatics_data = [{"category": "mathmatics", "title": "双子素数は無限に存在するか"}]
+mathmatics_data = [{"category": "mathmatics", "title": "双子素数は無限に存在するか"},
+                   {"category": "mathmatics", "title": "ホッジ予想"},
+                   {"category": "mathmatics", "title": "リーマン予想"},
+                   {"category": "mathmatics", "title": "ヤン–ミルズ方程式と質量ギャップ問題"},
+                   {"category": "mathmatics", "title": "ナビエ–ストークス方程式の解の存在と滑らかさ"},
+                   {"category": "mathmatics", "title": "バーチ・スウィンナートン＝ダイアー予想"},
+                   {"category": "mathmatics", "title": "メルセンヌ素数は無限に存在するか"},
+                   {"category": "mathmatics", "title": "ボスト予想"},
+                   {"category": "mathmatics", "title": "コラッツの予想"},
+                   {"category": "mathmatics", "title": "ゴールドバッハの予想　"},
+                   ]
