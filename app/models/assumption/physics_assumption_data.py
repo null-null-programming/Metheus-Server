4173cd3fb19d74022939fa45d723df60f085a2dd
nullnull <nullnullprogramming@gmail.com>
2021-04-13 01:43:52 +0900

    add physics-data

--- app/models/assumption/physics_assumption_data.py
+++ app/models/assumption/physics_assumption_data.py
@@ -1,1 +1,11 @@
-physics_data = [{"category": "physics", "title": "ニュートリノに質量を与える仕組みはいったい何か？"}]
+physics_data = [{"category": "physics", "title": "宇宙の終焉"},
+                {"category": "physics", "title": "ダークマター"},
+                {"category": "physics", "title": "量子重力理論"},
+                {"category": "physics", "title": "ブラックホール情報パラドックス"},
+                {"category": "physics", "title": "階層性問題"},
+                {"category": "physics", "title": "超新星爆発"},
+                {"category": "physics", "title": "大統一理論"},
+                {"category": "physics", "title": "アモルファス固体"},
+                {"category": "physics", "title": "宇宙定数"},
+                {"category": "physics", "title": "アクシオン"},
+                ]
